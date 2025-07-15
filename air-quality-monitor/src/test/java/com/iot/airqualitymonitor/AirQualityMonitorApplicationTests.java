cfa309061fe9eac9d61a592dee093e6f61375405
Matheus <matheusdeoliveiraes@gmail.com>
2025-07-15 14:17:38 -0300

    saved main

---
+++ air-quality-monitor/src/test/java/com/iot/airqualitymonitor/AirQualityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.airqualitymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
