0e4b7b9d61317fa3161fd6f5f4664e57cddf934f
Matheus <matheusdeoliveiraes@gmail.com>
2025-08-01 16:55:38 -0300

    tá dando internal server error

--- air-quality-monitor/src/main/java/com/iot/airqualitymonitor/controller/AirQualityController.java
+++ air-quality-monitor/src/main/java/com/iot/airqualitymonitor/controller/AirQualityController.java
@@ -1,7 +1,9 @@
 package com.iot.airqualitymonitor.controller;
 
 import com.iot.airqualitymonitor.dto.AirPollutionResponse;
+import com.iot.airqualitymonitor.model.AirQualityData;
 import com.iot.airqualitymonitor.service.AirQualityService;
+import com.iot.airqualitymonitor.service.ThingsBoardHttpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AirQualityController {
 
-    private final AirQualityService airQualityService;
+    @Autowired
+    private AirQualityService airQualityService;
 
-    public AirQualityController(AirQualityService airQualityService) {
-        this.airQualityService = airQualityService;
+    private final ThingsBoardHttpService thingsBoardHttpService;
+
+    public AirQualityController(ThingsBoardHttpService thingsBoardHttpService) {
+        this.thingsBoardHttpService = thingsBoardHttpService;
     }
+
     @GetMapping("/api/openweather/airquality")
     public AirPollutionResponse getOpenWeatherAirQuality(@RequestParam double lat, @RequestParam double lon) {
         return airQualityService.getOpenWeatherAirQuality(lat, lon);
+    }
+
+    @GetMapping("/test-tb")
+    public String testThingsBoard() {
+        AirQualityData mockData = new AirQualityData();
+        mockData.setPm25(12.5);
+        mockData.setCo(0.8);
+        mockData.setO3(28.3);
+        mockData.setAqi(45);
+        mockData.setTemperature(25.0);
+
+        thingsBoardHttpService.sendTelemetry(mockData);
+        return "Dados enviados ao ThingsBoard!";
     }
 }
